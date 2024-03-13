54fd159e61372dbeab36f8a8478ac2140b1820be
uwuxverticuse <sagargusain150@gmail.com>
2024-03-13 14:41:38 +0530

    Strings in Typescript

---
+++ 03.String.ts
@@ -0,0 +1,13 @@
+
+
+let myName : string = "Hitesh";
+
+let myUser : {name : string,email : string }={
+    name : "Sagar",
+    email : "sagardummy@gmail.com",
+}
+console.log(myName);
+console.log(myUser.name);
+console.log(myUser.email);
+myUser.email.toLowerCase();
+console.log(myUser.password)
